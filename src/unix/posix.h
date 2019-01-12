715d39824f571e93cbbac4c1fa3dfa0f4cb54dcc
Edward Thomson <ethomson@edwardthomson.com>
2019-01-12 22:55:31 +0000

    regexec: use pcre as our fallback/builtin regex
    Use PCRE 8.42 as the builtin regex implementation, using its POSIX
    compatibility layer.  PCRE uses ASCII by default and the users locale
    will not influence its behavior, so its `regcomp` implementation is
    similar to `regcomp_l` with a C locale.

--- src/unix/posix.h
+++ src/unix/posix.h
@@ -96,26 +96,36 @@ GIT_INLINE(int) p_futimes(int f, const struct p_timeval t[2])
  * Otherwise, use our bundled PCRE implementation.
  */
 
-#define P_REG_EXTENDED REG_EXTENDED
-#define P_REG_ICASE REG_ICASE
-#define P_REG_NOMATCH REG_NOMATCH
-
-#define p_regex_t regex_t
-#define p_regmatch_t regmatch_t
-
-#define p_regerror regerror
-#define p_regexec regexec
-#define p_regfree regfree
-
 #ifdef GIT_USE_REGCOMP_L
-#include <xlocale.h>
+# include <xlocale.h>
+
+# define P_REG_EXTENDED REG_EXTENDED
+# define P_REG_ICASE REG_ICASE
+# define P_REG_NOMATCH REG_NOMATCH
+
+# define p_regex_t regex_t
+# define p_regmatch_t regmatch_t
 
 GIT_INLINE(int) p_regcomp(regex_t *preg, const char *pattern, int cflags)
 {
 	return regcomp_l(preg, pattern, cflags, (locale_t) 0);
 }
+# define p_regerror regerror
+# define p_regexec regexec
+# define p_regfree regfree
 #else
-# define p_regcomp regcomp
+# include "pcreposix.h"
+
+# define P_REG_EXTENDED PCRE_REG_EXTENDED
+# define P_REG_ICASE PCRE_REG_ICASE
+# define P_REG_NOMATCH PCRE_REG_NOMATCH
+
+# define p_regex_t pcre_regex_t
+# define p_regmatch_t pcre_regmatch_t
+# define p_regcomp pcre_regcomp
+# define p_regerror pcre_regerror
+# define p_regexec pcre_regexec
+# define p_regfree pcre_regfree
 #endif
 
 #endif
--- src/win32/posix.h
+++ src/win32/posix.h
@@ -13,6 +13,7 @@
 #include "path_w32.h"
 #include "utf-conv.h"
 #include "dir.h"
+# include "pcreposix.h"
 
 extern unsigned long git_win32__createfile_sharemode;
 extern int git_win32__retries;
@@ -61,16 +62,15 @@ extern struct tm * p_localtime_r(const time_t *timer, struct tm *result);
 extern struct tm * p_gmtime_r(const time_t *timer, struct tm *result);
 
 /* Use the bundled regcomp */
-#define P_REG_EXTENDED REG_EXTENDED
-#define P_REG_ICASE REG_ICASE
-#define P_REG_NOMATCH REG_NOMATCH
+#define P_REG_EXTENDED PCRE_REG_EXTENDED
+#define P_REG_ICASE PCRE_REG_ICASE
+#define P_REG_NOMATCH PCRE_REG_NOMATCH
 
-#define p_regex_t regex_t
-#define p_regmatch_t regmatch_t
-
-#define p_regcomp regcomp
-#define p_regerror regerror
-#define p_regexec regexec
-#define p_regfree regfree
+#define p_regex_t pcre_regex_t
+#define p_regmatch_t pcre_regmatch_t
+#define p_regcomp pcre_regcomp
+#define p_regerror pcre_regerror
+#define p_regexec pcre_regexec
+#define p_regfree pcre_regfree
 
 #endif
